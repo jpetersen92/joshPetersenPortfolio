ca289fe11b3ed0aae9104ae74fd8828401a6c6ab
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2021-10-20 12:26:05 -0700

    added function to mobile menu

--- app.js
+++ app.js
@@ -4,8 +4,20 @@
 
 const portApp = {};
 
-portApp.init = () => {
+portApp.show = () => {
+    const barMenuButton = document.querySelector('.mobileNav');
+    const nav = document.getElementById('sidebar')
+    const hero = document.querySelector('.heroImage');
+    barMenuButton.addEventListener('click', () => {
+        barMenuButton.classList.toggle('show')
+        nav.classList.toggle('show')
+        hero.classList.toggle('expandHero')
+    })
+}
 
+portApp.init = () => {
+    console.log('were moving');
+    portApp.show();
 }
 
 AOS.init();
